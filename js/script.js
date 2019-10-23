09ddb462699da29763c31e9bae12931d63895fdf
bassiebangura <devbassiebangura@gmail.com>
2019-10-23 16:26:55 -0400

    Working version of T-shirt design selection option

---
+++ js/script.js
@@ -0,0 +1,54 @@
+$("#name").focus(); //add focus to name input on page load
+
+$("#other-title").hide();
+// cornflowerblue;
+//cornflowerblue
+// script.js:16 darkslategrey
+// script.js:16 gold
+// script.js:16 tomato
+// script.js:16 steelblue
+// script.js:16 dimgrey
+
+$("#color option").each(function(index, item) {
+  $(item).hide();
+});
+
+//add change() to Select Theme And perform the appropiate changes on
+//color to select for T-shirt based on design option selected
+$("#design").change(function() {
+  let selectedDesign = $("select#design")
+    .children("option:selected")
+    .val();
+
+  //   console.log(selectedDesign);
+  if (selectedDesign === "js puns") {
+    $("#color option:lt(4)").each(function(index, item) {
+      $(item).show();
+    });
+
+    $("#color option:gt(3)").each(function(index, item) {
+      $(item).hide();
+    });
+    $("#color option[value='']").hide();
+    $("#color option[value='tomato']").attr("selected", false);
+    $("#color option[value='cornflowerblue']").attr("selected", true);
+  } else if (selectedDesign === "heart js") {
+    $("#color option:lt(4)").each(function(index, item) {
+      $(item).hide();
+    });
+
+    $("#color option:gt(3)").each(function(index, item) {
+      $(item).show();
+    });
+    $("#color option[value='']").hide();
+    $("#color option[value='cornflowerblue']").attr("selected", false);
+    $("#color option[value='tomato']").attr("selected", true);
+  } else {
+    $("#color option").each(function(index, item) {
+      $(item).hide();
+    });
+    $("#color option[value='cornflowerblue']").attr("selected", false);
+    $("#color option[value='tomato']").attr("selected", false);
+    $("#color option[value='']").attr("selected", true);
+  }
+});
